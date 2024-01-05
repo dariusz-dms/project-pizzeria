58f2707b1aa3b8e7f7e059e5b86c2789ed97641a
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2024-01-06 00:22:08 +0100

    accordion

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,7 @@ const select = {
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new Product:', thisProduct);
     }
@@ -75,6 +76,25 @@ const select = {
       const menuContainer = document.querySelector(select.containerOf.menu);
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+    }
+    
+    initAccordion() {
+      const thisProduct = this;
+
+       /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = ???;
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
+
+        /* find active product (product that has active class) */
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+
+        /* toggle active class on thisProduct.element */
+      });
+      
     }
   }
 
