d0db9bc47fb5851829a25551cc732c41e1465bdf
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2024-01-06 01:23:34 +0100

    price3

--- src/js/script.js
+++ src/js/script.js
@@ -145,7 +145,14 @@ const select = {
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+            if (!option.default) {
+              price += option.price;
+            }
+          } else {
+            if (option.default) {
+              price -= option.price;
+            }
         }
       }
     
