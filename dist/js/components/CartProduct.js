44565361461c1d9cac3ac11a8d160da3b170197e
dariusz-dms <150970068+dariusz-dms@users.noreply.github.com>
2024-01-23 12:30:36 +0100

    fix code

--- dist/js/components/CartProduct.js
+++ dist/js/components/CartProduct.js
@@ -11,12 +11,12 @@ class CartProduct {
       thisCartProduct.amount = menuProduct.amount;
       thisCartProduct.priceSingle = menuProduct.priceSingle;
       thisCartProduct.price = menuProduct.price;
-      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+      thisCartProduct.params = { ...menuProduct.params }; // Używam spread operatora do klonowania obiektu
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
       thisCartProduct.initActions();
-    } 
+    }
   }
 
   getData() {
@@ -28,7 +28,7 @@ class CartProduct {
       price: thisCartProduct.price,
       priceSingle: thisCartProduct.priceSingle,
       name: thisCartProduct.name,
-      params: thisCartProduct.params,
+      params: { ...thisCartProduct.params }, // Używam spread operatora do klonowania obiektu
     };
 
     return productData;
@@ -43,11 +43,6 @@ class CartProduct {
     thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
     thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
     thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
-
-    thisCartProduct.dom.deliveryFee = document.querySelector(select.cart.deliveryFee);
-    thisCartProduct.dom.subtotalPrice = document.querySelector(select.cart.subtotalPrice);
-    thisCartProduct.dom.totalPrice = document.querySelectorAll(select.cart.totalPrice);
-    thisCartProduct.dom.totalNumber = document.querySelector(select.cart.totalNumber);
   }
 
   initAmountWidget() {
@@ -64,10 +59,6 @@ class CartProduct {
       thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amountWidget.value;
 
       thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
-
-      if (thisCartProduct.product) {
-        thisCartProduct.product.update();
-       }
     });
   }
 
@@ -87,23 +78,18 @@ class CartProduct {
 
   remove() {
     const thisCartProduct = this;
-  
+
     const event = new CustomEvent('remove', {
       bubbles: true,
       detail: {
         cartProduct: thisCartProduct,
       },
     });
-  
+
     thisCartProduct.dom.wrapper.dispatchEvent(event);
-  
+
     console.log('Product removed:', thisCartProduct);
-  
-    if (thisCartProduct.product) {
-      thisCartProduct.product.update();
-    }
   }
-  
 }
 
 export default CartProduct;
